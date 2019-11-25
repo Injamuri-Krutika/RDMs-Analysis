dbdcc72d676707d4a04dcc7a650e221a0961b00f
Krutika Injamuri <kruteeka.injamuri@gmail.com>
2019-11-25 06:20:54 +0530

    Add category labels to y-axis

--- src/lib/rsa/rdms.py
+++ src/lib/rsa/rdms.py
@@ -6,9 +6,8 @@ from lib.utils.plot import plot_rdm_group
 class GenerateRDMs:
     def __init__(self, config):
         self.config = config
-        print(self.config)
 
-    def rdm(self, data, roi, path):
+    def rdm(self, data, roi, path, labels):
         rdm = {}
         for distance_measure in self.config.distance_measures:
             if distance_measure == "pearson":
@@ -17,4 +16,4 @@ class GenerateRDMs:
             elif distance_measure == "kernel":
                 rdm["kernel"] = 1 - gaussian_kernal_similarity(data)
 
-        plot_rdm_group(rdm, roi, self.config.distance_measures, path)
+        plot_rdm_group(rdm, roi, self.config.distance_measures, path, labels)
--- src/lib/utils/plot.py
+++ src/lib/utils/plot.py
@@ -1,25 +1,38 @@
 import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 import os
+import numpy as np
+from scipy.stats import spearmanr
 
 
-def plot_rdm_group(rdms, roi, distance_measures, path):
-    print(distance_measures)
+def plot_rdm_group(rdms, roi, distance_measures, path, labels):
     subplot_2 = [121, 122]
-    subplot_3 = [131, 132, 133]
+#     subplot_3 = [131, 132, 133]
     fig = plt.figure(figsize=(16, 12))
-    for distance in distance_measures:
-        subplot_nums = subplot_2 if len(distance_measures) == 2 else subplot_3
-        for i, subplot_num in enumerate(subplot_nums):
-            distance = distance_measures[i]
-            ax = fig.add_subplot(subplot_num)
-            print("distance :", distance)
-            im = ax.imshow(rdms[distance],
-                           interpolation='None', cmap='bwr',)
-            ax.set_title('RDM, %s, distance: %s,' % (roi, distance))
 
-            divider = make_axes_locatable(ax)
-            cax = divider.append_axes('right', size='5%', pad=0.05)
-            fig.colorbar(im, cax=cax, orientation='vertical')
-    print(path+"/"+roi+".png")
+    distance = distance_measures[0]
+    ax1 = fig.add_subplot(subplot_2[0])
+    im1 = ax1.imshow(rdms[distance],
+                     interpolation='None', cmap='bwr',)
+    ax1.set_title('RDM, %s, distance: %s,' % (roi, distance))
+    ax1.set_yticks(np.arange(len(labels)))
+    ax1.set_yticklabels(labels)
+    divider = make_axes_locatable(ax1)
+    cax = divider.append_axes('right', size='5%', pad=0.05)
+    fig.colorbar(im1, cax=cax, orientation='vertical')
+
+    distance = distance_measures[1]
+    ax2 = fig.add_subplot(subplot_2[1])
+    im2 = ax2.imshow(rdms[distance],
+                     interpolation='None', cmap='bwr',)
+    ax2.set_title('RDM, %s, distance: %s,' % (roi, distance))
+    ax2.set_yticks(np.arange(len(labels)))
+    ax2.set_yticklabels(labels)
+    divider = make_axes_locatable(ax2)
+    cax = divider.append_axes('right', size='5%', pad=0.05)
+    fig.colorbar(im2, cax=cax, orientation='vertical')
+    # print(spearmanr(
+    #     rdms[distance_measures[0]], rdms[distance_measures[1]])[0])
+
+    # plt.title("Correlation between two RDMs: ")
     plt.savefig(os.path.join(path, roi+".png"))
