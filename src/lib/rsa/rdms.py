a13f78ea5e4c02ac19ac924030f11caa6c621354
Krutika Injamuri <kruteeka.injamuri@gmail.com>
2019-11-27 11:29:37 +0530

    Add logic to calculate RDMs with Epanechnikov

--- src/lib/rsa/rdms.py
+++ src/lib/rsa/rdms.py
@@ -1,5 +1,7 @@
 import numpy as np
 from lib.distance_measures.gaussian_kernel import gaussian_kernal_similarity
+from lib.distance_measures.epanechnikov_kernel import epanechnikov_similarity
+
 from lib.utils.plot import plot_rdm_group
 
 
@@ -15,5 +17,8 @@ class GenerateRDMs:
 
             elif distance_measure == "kernel":
                 rdm["kernel"] = 1 - gaussian_kernal_similarity(data)
+
+            elif distance_measure == "epanechnicov":
+                rdm["epanechnicov"] = 1 - epanechnikov_similarity(data)
 
         plot_rdm_group(rdm, roi, self.config.distance_measures, path, labels)
